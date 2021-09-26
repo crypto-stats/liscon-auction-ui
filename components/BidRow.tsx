6b1f5d9f32b8013cf79d386443c5138232dc93a2
David Mihal <david@dmihal.com>
2021-09-26 20:01:33 +0100

    Update bids

--- components/BidRow.tsx
+++ components/BidRow.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CountUp from 'react-countup'
 import { useAuction, Bid } from 'state/auction'
 
 const BidRow: React.FC<{ bid: Bid }> = ({ bid }) => {
-  const { setBidApproval, update } = useAuction()
+  const [showUpdate, setShowUpdate] = useState(false)
+  const [newBid, setNewBid] = useState(bid.gweiPerSec)
+  const { setBidApproval, update, updateBid } = useAuction()
   
   return (
     <div>
@@ -20,6 +22,22 @@ const BidRow: React.FC<{ bid: Bid }> = ({ bid }) => {
           />
         ) : bid.balance}
       </div>
+
+      {showUpdate && (
+        <div>
+          New bid:
+          <input type="number" value={newBid} onChange={(e: any) => setNewBid(e.target.value)} />
+          <button onClick={async () => {
+            await updateBid(bid.id, newBid)
+            await update()
+            setShowUpdate(false)
+          }}>
+            Update
+          </button>
+          <button onClick={() => setShowUpdate(false)}>Cancel</button>
+        </div>
+      )}
+
       <div>
         <button onClick={async () => {
           await setBidApproval(bid.id, !bid.approved)
@@ -27,6 +45,7 @@ const BidRow: React.FC<{ bid: Bid }> = ({ bid }) => {
         }}>
           {bid.approved ? 'Unapprove' : 'Approve'}
         </button>
+        <button onClick={() => setShowUpdate(true)} disabled={showUpdate}>Update Bid</button>
       </div>
     </div>
   )
--- state/auction.tsx
+++ state/auction.tsx
@@ -20,6 +20,7 @@ export interface AuctionState {
 interface AuctionStateWithMethods extends AuctionState {
   addBid(owner: string, text: string, gweiPerSec: number, deposit: number): Promise<number>
   setBidApproval(id: number, approved: boolean): Promise<void>
+  updateBid(id: number, gweiPerSec: number): Promise<void>
   update(): Promise<void>
 }
 
@@ -34,6 +35,7 @@ const AuctionContext = React.createContext<AuctionStateWithMethods>({
   ...defaultState,
   async addBid() { throw new Error('Not initialized') },
   async setBidApproval() { throw new Error('Not initialized') },
+  async updateBid() { throw new Error('Not initialized') },
   async update() { throw new Error('Not initialized') },
 });
 
@@ -42,6 +44,73 @@ export const useAuction = () => useContext(AuctionContext)
 export const AuctionProvider: React.FC = ({ children }) => {
   const nextId = useRef(0)
   const [state, setState] = useState<AuctionState>(defaultState)
+
+  const updateState = (currentState: AuctionState) => {
+    let nextTopBid = currentState.activeBid
+    let ethCollected = currentState.ethCollected
+    const now = Date.now() / 1000
+
+    if (currentState.activeBid) {
+      let amountPaid = currentState.activeBid.gweiPerSec * (now - currentState.lastUpdate) / 1e9
+      if (amountPaid > currentState.activeBid.balance) {
+        amountPaid = currentState.activeBid.balance
+
+        currentState.activeBid.active = false
+        nextTopBid = null
+      }
+
+      currentState.activeBid.balance -= amountPaid
+      ethCollected += amountPaid
+    }
+
+    if (nextTopBid && !nextTopBid.approved) {
+      nextTopBid = null
+    }
+
+    for (const bid of currentState.bids) {
+      if (bid.approved
+        && bid.balance > 0
+        && (!nextTopBid || bid.gweiPerSec > nextTopBid.gweiPerSec)) {
+        nextTopBid = bid
+      }
+    }
+    if (nextTopBid !== currentState.activeBid) {
+      if (nextTopBid) {
+        nextTopBid.active = true
+      }
+      if (currentState.activeBid?.active) {
+        currentState.activeBid.active = false
+      }
+    }
+    return {
+      ...currentState,
+      ethCollected,
+      lastUpdate: now,
+      activeBid: nextTopBid,
+      bids: [...currentState.bids],
+    }
+  }
+
+  const changeBid = (id: number, update: any, withUpdate?: boolean) =>
+    setState((currentState: AuctionState) => {
+      for (const i in currentState.bids) {
+        const _state = withUpdate ? updateState(currentState) : currentState
+
+        if (_state.bids[i].id === id) {
+          const newBid = typeof update === 'function'
+            ? update(state.bids[i])
+            : { ...state.bids[i], ...update }
+
+          _state.bids[i] = newBid
+
+          const activeBid = _state.activeBid?.id === id
+            ? newBid
+            : _state.activeBid
+          return { ..._state, activeBid }
+        }
+      }
+      throw new Error(`Bid ${id} not found`)
+    })
 
   return (
     <AuctionContext.Provider value={{
@@ -62,65 +131,21 @@ export const AuctionProvider: React.FC = ({ children }) => {
         }))
         return id
       },
+      
       async setBidApproval(id: number, approved: boolean) {
-        for (const i in state.bids) {
-          if (state.bids[i].id === id) {
-            state.bids[i] = { ...state.bids[i], approved }
-            const activeBid = state.activeBid?.id === id ? state.bids[i] : state.activeBid
-            setState({ ...state, activeBid })
-            return
-          }
-        }
-        throw new Error(`Bid ${id} not found`)
+        changeBid(id, { approved })
       },
+
+      async updateBid(id: number, gweiPerSec: number) {
+        changeBid(id, { gweiPerSec }, true)
+      },
+
+      // async deposit(id: number, ethToDeposit: number) {
+      //   changeBid(id, { gweiPerSec }, true)
+      // },
+
       async update() {
-        setState((currentState: AuctionState) => {
-          let nextTopBid = currentState.activeBid
-          let ethCollected = currentState.ethCollected
-          const now = Date.now() / 1000
-
-          if (currentState.activeBid) {
-            let amountPaid = currentState.activeBid.gweiPerSec * (now - currentState.lastUpdate) / 1e9
-            if (amountPaid > currentState.activeBid.balance) {
-              amountPaid = currentState.activeBid.balance
-
-              currentState.activeBid.active = false
-              nextTopBid = null
-            }
-
-            currentState.activeBid.balance -= amountPaid
-            ethCollected += amountPaid
-          }
-
-          if (nextTopBid && !nextTopBid.approved) {
-            nextTopBid = null
-          }
-
-          for (const bid of currentState.bids) {
-            if (bid.approved
-              && bid.balance > 0
-              && (!nextTopBid || bid.gweiPerSec > nextTopBid.gweiPerSec)) {
-              nextTopBid = bid
-            }
-          }
-          if (nextTopBid !== currentState.activeBid) {
-            if (nextTopBid) {
-              nextTopBid.active = true
-            }
-            if (currentState.activeBid?.active) {
-              currentState.activeBid.active = false
-            }
-
-            return {
-              ...currentState,
-              ethCollected,
-              lastUpdate: now,
-              activeBid: nextTopBid,
-              bids: [...currentState.bids],
-            }
-          }
-          return currentState
-        })
+        setState(updateState)
       },
     }}>
       {children}
