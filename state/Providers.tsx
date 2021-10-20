62ea987eac6a7406cca1d63b5ead0d90720a26dd
David Mihal <david@dmihal.com>
2021-10-20 10:32:29 +0100

    Plausible

--- state/Providers.tsx
+++ state/Providers.tsx
@@ -3,6 +3,7 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { ethers } from 'ethers'
 import { AccountsProvider } from './accounts'
 import { AuctionProvider } from './auction'
+import PlausibleProvider from 'next-plausible'
 
 const Providers: React.FC<{ mode: 'memory' | 'testnet' }> = ({ children, mode }) => {
   const Web3Provider = mode === 'testnet' ? Web3ReactProvider : Fragment;
@@ -11,7 +12,9 @@ const Providers: React.FC<{ mode: 'memory' | 'testnet' }> = ({ children, mode })
     <Web3Provider getLibrary={(provider: any) => new ethers.providers.Web3Provider(provider)}>
       <AccountsProvider mode={mode}>
         <AuctionProvider mode={mode}>
-          {children}
+          <PlausibleProvider domain="stream.liscon.org">
+            {children}
+          </PlausibleProvider>
         </AuctionProvider>
       </AccountsProvider>
     </Web3Provider>
